e464ebcd5384f7fa824759da1ff30472a8339568
Splintex <eugene.godun@gmail.com>
2015-06-18 12:24:25 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -1,8 +1,46 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+	$(document).on("click", function(){
+		if ($(".js-search").hasClass("is-active")) {
+			$(".js-search").removeClass("is-active");
+		}
+		
+	});
+
+// get scroll width;
+	var scrollWidth;
+	function getScrollBarWidth() {
+	  var inner = document.createElement('p');
+	  inner.style.width = "100%";
+	  inner.style.height = "200px";
+
+	  var outer = document.createElement('div');
+	  outer.style.position = "absolute";
+	  outer.style.top = "0px";
+	  outer.style.left = "0px";
+	  outer.style.visibility = "hidden";
+	  outer.style.width = "200px";
+	  outer.style.height = "150px";
+	  outer.style.overflow = "hidden";
+	  outer.appendChild (inner);
+
+	  document.body.appendChild (outer);
+	  var w1 = inner.offsetWidth;
+	  outer.style.overflow = 'scroll';
+	  var w2 = inner.offsetWidth;
+	  if (w1 == w2) w2 = outer.clientWidth;
+
+	  document.body.removeChild (outer);
+	  scrollWidth = w1 - w2;
+	  return scrollWidth;
+
+	}
+	getScrollBarWidth();
+
+
+
+	var header = $(".js-header");
+	var body = $("body");
 
 	document.createElement( "picture" );
 	var config = {
@@ -52,7 +90,55 @@ head.ready(function() {
 		if ($(window).width() >= 1024 && $(".js-post-slider").hasClass(".slick-slider"))  {
 			$(".js-post-slider").slick("unslick");
 		}
-	})
+	});
+
+	$(".js-search").on('click', function(event) {
+		event.stopPropagation();
+	});
+
+	$('.js-toggle-search').on('click', function() {
+		$(".js-search").toggleClass("is-active");
+		return false;
+	});
+
+
+	$('.js-toggle-menu').on('click', function() {
+		if ($(this).hasClass("is-active")) {
+			$(".js-menu").hide();
+			body.removeClass("has-open-nav").css({
+				marginRight: 0
+			});
+			$(this).removeClass("is-active");
+		}
+		else {
+			$(".js-menu").show();
+			body.toggleClass("has-open-nav").css({
+				marginRight: scrollWidth
+			});
+			$(this).addClass("is-active");
+		}
+		
+		return false;
+	});
+	
+	function fixHeader() {
+		var scrollTop = $(window).scrollTop();
+		var height = header.outerHeight();
+		if (scrollTop >= height) {
+			body.addClass("has-fixed-header");
+		}
+		else {
+			body.removeClass("has-fixed-header");
+		}
+	}
+	fixHeader();
+
+	$(window).scroll(function() {
+		fixHeader();
+	});
+	
+
+
 
 
 	
