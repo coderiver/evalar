aa3141d98ef4903f546a75d817f4a381ebd710c7
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2015-09-25 13:47:11 +0300

    allpages upd

--- js/common.js
+++ js/common.js
@@ -622,5 +622,39 @@ $(document).ready(function() {
         $('.js-carousel .meta > div').removeClass('is-active');
         $('.js-carousel .meta > div').eq(fotorama.activeFrame.i-1).addClass('is-active');
      }).fotorama();
-
+       //  function tabsLoad() {
+       //      var hash = window.location.hash;
+       //      if (hash) {
+       //          $('[href="'+hash+'"]').parents(".js-tabs-group").find(".js-tabs-content").hide();
+       //          $('[data-id="'+hash+'"]').show();
+       //          $('[href="'+hash+'"]').parents(".js-tabs").find("li").removeClass("is-active");
+       //          $('[href="'+hash+'"]').parent().addClass("is-active");
+       //      }
+       //      else if ($(".js-tabs li.is-active").length) {
+       //          $(".js-tabs li").each(function(){
+       //              if ($(this).hasClass("is-active")) {
+       //                 var hash = $(this).find("a").attr("href");
+       //                  $('[data-id="'+hash+'"]').show();
+       //                  window.location.hash = hash;
+       //              }
+       //          });
+       //      }
+       //      else {
+       //          $('.js-tabs li:first').addClass("is-active");
+       //          $('.js-tabs').next().show();
+                
+       //      }
+            
+       //  }
+       // tabsLoad();
+       // // alert(hash);
+       //  $('.js-tabs a').on("click",function (e) {
+       //      var content = $(this).attr("href");
+       //      $(this).parents(".js-tabs").find("li").removeClass("is-active");
+       //      $(this).parent().addClass("is-active");
+       //      $(this).parents(".js-tabs-group").find(".js-tabs-content").hide();
+       //      $('[data-id="'+content+'"]').show();
+       //      window.location.hash = this.hash;
+       //      return false;
+       //  });
 });
